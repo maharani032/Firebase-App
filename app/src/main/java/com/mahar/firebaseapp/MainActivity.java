7703a1ebeebd47d61e79f4fdc0ccfd4fa6bd403a
Maharani Nirmala Dewi <maharanisakti015@Gmail.com>
2022-08-16 15:58:00 +0700

    signout, getcurrentuser, navigation

--- app/src/main/java/com/mahar/firebaseapp/MainActivity.java
+++ app/src/main/java/com/mahar/firebaseapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mahar.firebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,12 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button signInButton,signUpButton;
     private EditText email,password;
     private TextView labelEmail,labelPassword;
+
+    FirebaseAuth auth=FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String userMail=email.getText().toString();
+                String userPassword=password.getText().toString();
+                signInFirebase(userMail,userPassword);
 
             }
         });
@@ -55,5 +68,35 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(signUpActivity);
             }
         });
+    }
+    public void signInFirebase(String email,String password){
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Intent i = new Intent(MainActivity.this,HomePage.class);
+                            startActivity(i);
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this,"Mail or Password is not correct",Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user=auth.getCurrentUser();
+        if(user!=null){
+            Intent i = new Intent(MainActivity.this,HomePage.class);
+            startActivity(i);
+            finish();
+        }
     }
 }
---
+++ app/src/main/java/com/mahar/firebaseapp/ui/setting/SettingFragment.java
@@ -0,0 +1,56 @@
+package com.mahar.firebaseapp.ui.setting;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.mahar.firebaseapp.MainActivity;
+import com.mahar.firebaseapp.databinding.FragmentSettingBinding;
+
+public class SettingFragment extends Fragment {
+
+    private FragmentSettingBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        SettingViewModel settingViewModel =
+                new ViewModelProvider(this).get(SettingViewModel.class);
+
+        binding = FragmentSettingBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+//?        final TextView textView = binding.textDashboard;
+        final LinearLayout signOut=binding.logoutCard;
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(getActivity(),"Should be logout",Toast.LENGTH_SHORT).show();
+                FirebaseAuth.getInstance().signOut();
+                Intent i= new Intent(getActivity(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+//?        settingViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
+
+        return root;
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
